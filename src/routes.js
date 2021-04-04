e6eae6d5331675ec5ed7c160b09f078648cfe317
Melo-Lucas <lucs.r06@gmail.com>
2021-04-04 01:29:37 -0300

    Rotas criadas e definidas de acordo com o projeto

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,9 @@ import DisciplinaController from "./app/controllers/DisciplinaController"
 
 const routes = new Router();
 
+routes.post("/disciplina", DisciplinaController.create)
+routes.get("/disciplina", DisciplinaController.read)
+routes.put("/disciplina/:id", DisciplinaController.update)
+routes.delete("/disciplina/:id", DisciplinaController.delete)
+
 export default routes;
